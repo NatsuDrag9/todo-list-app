92bff39fec5a83d3ec1c7adb3b03fb0a32712b91
NatsuDrag9 <rohitimandi9@zoho.com>
2023-12-08 13:34:09 +0100

    Moved api tests to ToDoList.test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App.tsx";
 import { beforeEach, describe, expect, test } from "vitest";
-import { fetchTasks } from "../utils/apiCalls.ts";
-
-// type FetchMock = (input: RequestInfo, init?: RequestInit) => Promise<Response>;
 
 describe("Initial render", () => {
   beforeEach(() => {
@@ -16,45 +13,4 @@ describe("Initial render", () => {
   })
 });
 
-describe("Test API Call", () => {
-  test("fetchTasks returns a list of tasks on successful fetch", async () => {
-    // Mock the global fetch function
-    globalThis.fetch = vi.fn(() =>
-      Promise.resolve({
-        ok: true,
-        json: () =>
-          Promise.resolve({
-            tasks: [{ name: "Task 1", tag: "Tag1", status: "Incomplete" }],
-          }),
-      })
-    ) as any;
 
-    // The global fetch function used in fetchTasks is replaced by the mock
-    // -- globalThis.fetch
-    const tasks = await fetchTasks();
-    expect(tasks).toEqual([
-      { name: "Task 1", tag: "Tag1", status: "Incomplete" },
-    ]);
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
-
-  test("fetchTasks handles fetch failure", async () => {
-    // Mock the global fetch function
-    globalThis.fetch = vi.fn(() =>
-      Promise.reject(new Error("Network failure"))
-    ) as any;
-
-    // The global fetch function used in fetchTasks is replaced by the mock
-    // -- globalThis.fetch
-    try {
-      await fetchTasks();
-      /* This code should not be reached because the fetch request is expected to fail.
-      The line below will cause intentional failure and can be removed. This is a way 
-      to signal that the code should not reach this point */
-    //   expect(true).toBe(false);
-    } catch (error) {
-      expect(error).toEqual(new Error("Network failure"));
-    }
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
-});
--- src/test/TodoList.test.tsx
+++ src/test/TodoList.test.tsx
@@ -45,3 +45,46 @@ describe("Initial render", () => {
     expect(rightArrowElement).toBeDefined();
   });
 });
+
+describe("Test API Call", () => {
+  test("fetchTasks returns a list of tasks on successful fetch", async () => {
+    // Mock the global fetch function
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            tasks: [{ name: "Task 1", tag: "Tag1", status: "Incomplete" }],
+          }),
+      })
+    ) as any;
+
+    // The global fetch function used in fetchTasks is replaced by the mock
+    // -- globalThis.fetch
+    const tasks = await fetchTasks();
+    expect(tasks).toEqual([
+      { name: "Task 1", tag: "Tag1", status: "Incomplete" },
+    ]);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test("fetchTasks handles fetch failure", async () => {
+    // Mock the global fetch function
+    globalThis.fetch = vi.fn(() =>
+      Promise.reject(new Error("Network failure"))
+    ) as any;
+
+    // The global fetch function used in fetchTasks is replaced by the mock
+    // -- globalThis.fetch
+    try {
+      await fetchTasks();
+      /* This code should not be reached because the fetch request is expected to fail.
+      The line below will cause intentional failure and can be removed. This is a way 
+      to signal that the code should not reach this point */
+    //   expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toEqual(new Error("Network failure"));
+    }
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
