7e40af844eeeb99e63dbf9638b093bf1eedf6137
NatsuDrag9 <rohitimandi9@zoho.com>
2023-12-11 11:42:47 +0100

    Changed base to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
     // since parsing CSS is slow
     css: true,
   },
-  base: "https://natsudrag9.github.io/todo-list-app/"
+  base: "https://natsudrag9.github.io/todo-list-app/" // Uncomment for production
 })
